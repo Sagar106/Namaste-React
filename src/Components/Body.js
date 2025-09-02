998f24179acbd2bf9bef49aed1d8a4595bc67f52
Sagar Upadhyay <sagarupadhyay106@gmail.com>
2025-09-03 00:03:49 +0530

    Shimmer UI and Swiggy Live API data fetching

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,13 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-    const [listOfRestaurants, setListOfRestaurants] = useState(resList)
+    const [listOfRestaurants, setListOfRestaurants] = useState([])
 
     const handleTopRatedButton = () => {
         const filteredList = listOfRestaurants.filter((item) => item.info.avgRating > 4)
         setListOfRestaurants(filteredList)
+    }
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.119463&lng=77.630658&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data?.json();
+
+        console.log("json", json)
+
+        setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    if (listOfRestaurants.length === 0) {
+        return <Shimmer />
     }
 
     return (
